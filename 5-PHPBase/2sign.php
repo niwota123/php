ab7ea4dc5ef1b232b53c98ea8535439f1b7bd921
superking <superwanc@gmail.com>
2017-07-27 14:37:06 +0800

    运算符课件

--- 5-PHPBase/2sign.php
+++ 5-PHPBase/2sign.php
@@ -1,11 +1,11 @@
 <?php
 
-header('content-type:text/html;charset=gb2312');
+// header('content-type:text/html;charset=gb2312');
 /**
  * @Author: superking
  * @Date:   2017-07-27 11:00:29
  * @Last Modified by:   superking
- * @Last Modified time: 2017-07-27 11:59:09
+ * @Last Modified time: 2017-07-27 14:36:21
  */
 
 //运算
@@ -95,6 +95,63 @@ if (@$a['a']) {
 }
 
 // 执行运算符
-echo `ipconfig`;
+//echo `ipconfig`;
 //通过函数的方式执行名利
-echo shell_exec('dir');
+// echo shell_exec('dir');
+
+//自增,自减
+echo '<hr>';
+$a = 0;
+$a++;//自己增加1
+echo $a;
+$a--;//自己减1
+echo $a;
+
+
+echo '<hr>';
+// 前置 直接加1
+$b = 10;
+echo ++$b;//11
+
+// 后置,执行完语句再加1
+$b = 10;
+echo $b++;//10
+echo $b;
+
+//逻辑运算符
+// and &&  逻辑与
+// or  ||  逻辑或
+// xor     逻辑异或
+// not !   逻辑非
+
+echo '<hr>';
+var_dump(true and true); //true
+var_dump(false and true); //false
+var_dump(true && true); //true
+var_dump(false && true);//false
+
+echo '<hr>';
+var_dump(true or true);//true
+var_dump(true or false);//true
+var_dump(false or false);//false
+
+echo '<hr>';
+var_dump(true xor true);//false
+var_dump(true xor false);//true
+var_dump(false xor false);//false
+
+echo'<hr>';
+var_dump(!true);//false
+var_dump(!false);//true
+
+//字符串链接符号 .
+echo '<hr>';
+$a = '我是一个字符串';
+$b = 10;
+$c = $a.$b;
+echo $c;//
+
+//等效代码
+$c .= 'wo shi you zhui jia d  zi fu chuan ';
+$c = $c.'wo shi you zhui jia d  zi fu chuan ';
+echo $c;
