c626184fd7a3b7ef62326ac30a2b55b6499079ec
superking <superwanc@gmail.com>
2017-09-27 17:43:43 +0800

    tp-route

---
+++ 03-oop/06-thinkphp/composer/composerPrj.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/9/26
+ * Time: 10:20
+ */
+//空的工程
+
+//导入composer的自动加载工具
+require_once 'vendor\autoload.php';
+
+//$whoops = new \Whoops\Run;
+//$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+//$whoops->register();
+
+//错误的代码
+//adfasdfasd();
+
+dump($_SERVER);
